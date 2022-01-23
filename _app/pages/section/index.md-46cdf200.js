128278aae1433a0c8e3e39111c1ea9f01e103f8c
wighawag <wighawag@users.noreply.github.com>
2022-01-23 09:05:18 +0000

    deploy: a97234d0869dd4e04ea2934b2e1f559a44e304c4

--- _app/pages/section/index.md-46cdf200.js
+++
@@ -1,2 +0,0 @@
-import{S as c,i as r,s as l,e as d,t as p,c as f,a as m,g as u,d as o,f as _,H as h,I as s}from"../../chunks/vendor-7856393e.js";function x(i){let e,n;return{c(){e=d("p"),n=p("index of section")},l(t){e=f(t,"P",{});var a=m(e);n=u(a,"index of section"),a.forEach(o)},m(t,a){_(t,e,a),h(e,n)},p:s,i:s,o:s,d(t){t&&o(e)}}}class S extends c{constructor(e){super();r(this,e,null,x,l,{})}}export{S as default};
-//# sourceMappingURL=index.md-46cdf200.js.map
